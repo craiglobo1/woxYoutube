cae9c8c973582378db2eb04432a20666bcff46ee
Craig lobo <craiglobo1@gmail.com>
2020-08-24 16:04:34 +0400

    final v1

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 from wox import Wox, WoxAPI
 import webbrowser as wb
+import requests
 
 
 class Main(Wox):
 
     def query(self, key):
-        results = []
-        results.append({
-            "Title": "Hello world",
-            "SubTitle": 'Enter to Initiate Site',
-            "IcoPath": "images\\music.ico",
-            "JsonRPCAction": {
-                "method": "openUrl",
-                "parameters": [key],
-                "dontHideAfterAction": False
-            }
-        })
+        return self.convToDictionary(self.getResults(key))
 
+    def getResults(self, searchTerm):
+        apiKey = "AIzaSyBr3GzNXwL3Ps31sGUNPYU1WQAw02Rldr0"
+        url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={searchTerm}&key={apiKey}'
+        headers = {'content-type': 'application/json',
+                   'Accept-Charset': 'UTF-8'}
+        r = requests.get(url, headers=headers)
+        results = r.json()['items']
+        ids = [val['id']['videoId'] for val in results]
+        thumbnails = [val['snippet']['thumbnails']['medium']['url']
+                      for val in results]
+        titles = [val['snippet']['title'] for val in results]
+        # print(ids, '\n', thumbnails, '\n', titles)
+
+        return titles, ids, thumbnails
+
+    def convToDictionary(self, titles, ids, thumbnails):
+        results = []
+        for i in range(len(titles)):
+            results.append({
+                "Title": titles[i],
+                "SubTitle": 'Enter to Initiate Site',
+                "IcoPath": thumbnails[i],
+                "JsonRPCAction": {
+                    "method": 'openUrl',
+                    "parameters": [ids[i]],
+                    "dontHideAfterAction": False
+                }
+
+            })
         return results
 
-    def openUrl(self, url):
-        pass
+    def openUrl(self, ids):
+        wb.open(f'https://www.youtube.com/watch?v={ids}')
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,12 +1,40 @@
+
+from re import search
 import webbrowser as wb
-import os
-
-# SearchUrl="https://www.youtube.com/results?search_query="
-
-# opt = input('Enter your presence')
-
-# if opt == ('Youtube'):
-#     term = input("Enter Search Query: ")
-#     wb.open(SearchUrl+term,new=new)
+import requests
 
 
+def getResults(searchTerm):
+    apiKey = "AIzaSyBr3GzNXwL3Ps31sGUNPYU1WQAw02Rldr0"
+    url = f'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={searchTerm}&key={apiKey}'
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+    r = requests.get(url, headers=headers)
+    results = r.json()['items']
+    ids = [val['id']['videoId'] for val in results]
+    thumbnails = [val['snippet']['thumbnails']['medium']['url']
+                  for val in results]
+    titles = [val['snippet']['title'] for val in results]
+    # print(ids, '\n', thumbnails, '\n', titles)
+
+    return titles, ids, thumbnails
+
+titles, ids, thumbnails = getResults(searchTerm)
+
+def convToDictionary(titles,ids,thumbnails):
+    results = []
+    for i in range(len(titles)):
+        results.append({
+            "Title": titles[i],
+            "SubTitle": 'Enter to Initiate Site',
+            "IcoPath": thumbnails[i],
+            "JsonRPCAction": {
+                "method": openUrl,
+                "parameters": [ids[i]],
+                "dontHideAfterAction": False
+            }
+
+        })
+    return results
+
+def openUrl(ids):
+    wb.open(f'https://www.youtube.com/watch?v={ids}')
